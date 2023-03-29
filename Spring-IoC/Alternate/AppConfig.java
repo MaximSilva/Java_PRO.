956b66d81693bc451b6c700cdc34f5d5d94bcdce
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-03-29 09:22:50 +0300

    Add files via upload

---
+++ Spring-IoC/Alternate/AppConfig.java
@@ -0,0 +1,35 @@
+package ua.kiev.prog;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("ua.kiev.prog")
+public class AppConfig {
+    // comment out this bean to test the result
+    //@Bean
+    //public Preprocessor datePreprocessor() {
+     //   return new DatePreprocessor();
+    //}
+
+    @Bean(initMethod = "open", destroyMethod = "close")
+    public LoggerAPI fileLoggerAPI() {
+        return new FileLoggerAPI("log.txt");
+    }
+
+    //@Bean(name="consoleLogger")
+    //public LoggerAPI consoleLoggerAPI() {
+        //return new ConsoleLoggerAPI();
+    //}
+
+    /*@Bean
+    public Notifier notifier(@Qualifier("fileLoggerAPI") LoggerAPI api) {
+        return new Notifier(api);
+    }*/
+}
+
+
+
---
+++ SpringMVCDemo2/src/main/java/ua/kiev/prog/GroupRepository.java
@@ -0,0 +1,9 @@
+package ua.kiev.prog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
+
+
+
